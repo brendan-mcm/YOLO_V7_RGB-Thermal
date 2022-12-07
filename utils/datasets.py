95c3a80941a7603ab21754452680712e69bf0a9a
brendan-mcm <brendanmcmanamon@gmail.com>
2022-12-06 22:11:34 -0500

    print

--- utils/datasets.py
+++ utils/datasets.py
@@ -773,9 +773,9 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
 
         # Convert
         print("labels = "+str(self.labels))
-        print("img before mod .shape = "+img.shape)
+        print("img before mod .shape = "+str(img.shape))
         img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
-        print("img post mod .shape = "+img.shape)
+        print("img post mod .shape = "+str(img.shape))
         img = np.ascontiguousarray(img)
 
         return torch.from_numpy(img), labels_out, self.img_files[index], shapes
