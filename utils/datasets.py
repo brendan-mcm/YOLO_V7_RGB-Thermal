dc12a6c98584ac4c1d3de00195cb8b393685e539
Brendan McManamon <brendanmcm@dynamic-oit-vapornet100-10-8-1-117.princeton.edu>
2023-02-20 20:40:54 -0500

    default no aug

--- utils/datasets.py
+++ utils/datasets.py
@@ -751,7 +751,7 @@ def load_fused(self, index):
 
 ## End of custom, begin std
 class LoadImagesAndLabels(Dataset):  # for training/testing
-    def __init__(self, path, img_size=640, batch_size=16, augment=True, hyp=None, rect=False, image_weights=False,
+    def __init__(self, path, img_size=640, batch_size=16, augment=False, hyp=None, rect=False, image_weights=False,
                  cache_images=False, single_cls=False, stride=32, pad=0.0, prefix=''):
         self.img_size = img_size
         self.augment = augment
