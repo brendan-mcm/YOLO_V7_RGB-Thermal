fac9b069e19d1177a70f63784e6dac40e31bc286
Brendan McManamon <brendanmcm@dynamic-oit-vapornet100-10-9-74-31.princeton.edu>
2023-03-28 11:17:54 -0400

    ens

--- utils/general.py
+++ utils/general.py
@@ -615,6 +615,7 @@ def mod_non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes
     nc = prediction.shape[2] - 5  # number of classes
     xc = prediction[..., 4] > conf_thres  # candidates
 
+    print("xc = ", xc)
     # Settings
     min_wh, max_wh = 2, 4096  # (pixels) minimum and maximum box width and height
     max_det = 300  # maximum number of detections per image
@@ -645,26 +646,27 @@ def mod_non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes
             continue
 
         # Compute conf
-        if nc == 1:
+        ''''if nc == 1:
             x[:, 5:] = x[:, 4:5] # for models with one class, cls_loss is 0 and cls_conf is always 0.5,
                                  # so there is no need to multiplicate.
-        else:
-            x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+        else:'''
+        x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
 
         # Box (center x, center y, width, height) to (x1, y1, x2, y2)
         box = xywh2xyxy(x[:, :4])
 
         # Detections matrix nx6 (xyxy, conf, cls)
-        print("x piror = ",x)
+        print("x piror = ",np.shape(x))
         if multi_label:
             i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
             x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
-        else:  # best class only
+        
+        ''' else:  # best class only
             conf, j = x[:, 5:].max(1, keepdim=True)
             
-            x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
+            x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres] '''
         
-        print("x post = ", x)
+        print("x post = ", np.shape(x))
 
         # Filter by class
         if classes is not None:
@@ -687,13 +689,14 @@ def mod_non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes
         i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
         if i.shape[0] > max_det:  # limit detections
             i = i[:max_det]
-        if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+        
+        '''if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
             # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
             iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
             weights = iou * scores[None]  # box weights
             x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
             if redundant:
-                i = i[iou.sum(1) > 1]  # require redundancy
+                i = i[iou.sum(1) > 1]  # require redundancy '''
 
         output[xi] = x[i]
         if (time.time() - t) > time_limit:
