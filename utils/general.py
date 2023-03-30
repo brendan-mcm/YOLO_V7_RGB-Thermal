7dfa83c27ce9b9f08a405e799e3515b0be685179
Brendan McManamon <brendanmcm@dynamic-oit-vapornet100-10-9-74-31.princeton.edu>
2023-03-30 10:11:54 -0400

    minor

--- utils/general.py
+++ utils/general.py
@@ -703,8 +703,8 @@ def mod_non_max_suppression(rgb_pred, th_pred, rgb_conf=0.25, th_conf=0.25, iou_
     time_limit = 10.0  # seconds to quit after
     redundant = False  # require redundant detections
     multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
-    merge = False  # use merge-NMS
-    soft = False # use soft nms
+    merge = True  # use merge-NMS
+    soft = True # use soft nms
 
     t = time.time()
     output = [torch.zeros((0, 6), device=prediction.device)] * prediction.shape[0]
